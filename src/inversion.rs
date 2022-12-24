6a85bc8e68415f7e618a75f6a6f33a132652ae82
ashpil <ashpil@pm.me>
2022-12-24 16:17:10 -0500

    implement inversion2d in terms of inversion1d

--- src/inversion.rs
+++ src/inversion.rs
@@ -44,85 +44,40 @@ impl Distribution1D for Inversion1D {
 }
 
 pub struct Inversion2D {
-    pub width: usize,
-    pub height: usize,
-
-    pub conditional_pdfs_integrals: Vec<Vec<f32>>,
-    pub conditional_cdfs: Vec<Vec<f32>>,
-
-    pub marginal_pdf_integral: Vec<f32>,
-    pub marginal_cdf: Vec<f32>,
+    pub marginal: Inversion1D,
+    pub conditional: Box<[Inversion1D]>,
 }
 
 impl Distribution2D for Inversion2D {
-    fn build(image: &[Vec<f32>]) -> Self {
-        let height = image.len();
-        let width = image[0].len();
-
-        let mut conditional_pdfs_integrals: Vec<Vec<f32>> = vec![vec![0.0; width + 1]; height];
-        let mut conditional_cdfs = vec![vec![0.0; width + 1]; height];
-
-        let mut marginal_pdf_integral = vec![0.0; height + 1];
-        let mut marginal_cdf = vec![0.0; height + 1];
-
-        for (row_index, row) in conditional_pdfs_integrals.iter_mut().enumerate() {
-            for (col_index, pixel) in row[0..width].iter_mut().enumerate() {
-                *pixel = image[row_index][col_index];
-            }
+    fn build(weights: &[Vec<f32>]) -> Self {
+        let mut conditional = Vec::with_capacity(weights.len());
+        let mut marginal_weights = Vec::with_capacity(weights.len());
+        for row in weights {
+            let table = Inversion1D::build(row);
+            marginal_weights.push(table.integral());
+            conditional.push(table);
         }
 
-        for (j, row) in conditional_pdfs_integrals.iter_mut().enumerate() {
-            for i in 1..width + 1 {
-                conditional_cdfs[j][i] = conditional_cdfs[j][i - 1] + row[i - 1] / width as f32;
-            }
-
-            row[width] = conditional_cdfs[j][width];
-            for i in 0..width + 1 {
-                conditional_cdfs[j][i] /= row[width];
-            }
-        }
-
-        for (i, v) in marginal_pdf_integral[..height].iter_mut().enumerate() {
-            *v = conditional_pdfs_integrals[i][width];
-        }
-
-        for i in 1..height + 1 {
-            marginal_cdf[i] = marginal_cdf[i - 1] + marginal_pdf_integral[i - 1] / height as f32;
-        }
-
-        marginal_pdf_integral[height] = marginal_cdf[height];
-        for i in 0..height + 1 {
-            marginal_cdf[i] /= marginal_pdf_integral[height];
-        }
+        let marginal = Inversion1D::build(&marginal_weights);
 
         Self {
-            width,
-            height,
-
-            conditional_pdfs_integrals,
-            conditional_cdfs,
-
-            marginal_pdf_integral,
-            marginal_cdf,
+            marginal,
+            conditional: conditional.into_boxed_slice(),
         }
     }
 
     fn sample(&self, [u, v]: [f32; 2]) -> (f32, [usize; 2]) {
-        fn find_interval(a: &[f32], val: f32) -> usize {
-            a.partition_point(|p| *p <= val) - 1
-        }
+        let (pdf_y, y) = self.marginal.sample(u);
+        let (pdf_x, x) = self.conditional[y].sample(v);
 
-        let offset_v = find_interval(&self.marginal_cdf, v);
-        let pdf_v = self.marginal_pdf_integral[offset_v] / self.marginal_pdf_integral[self.height];
-
-        let offset_u = find_interval(&self.conditional_cdfs[offset_v], u);
-        let pdf_u = self.conditional_pdfs_integrals[offset_v][offset_u] / self.conditional_pdfs_integrals[offset_v][self.width];
-
-        (pdf_v * pdf_u, [offset_u, offset_v])
+        (pdf_x * pdf_y, [x, y])
     }
 
     fn pdf(&self, [u, v]: [usize; 2]) -> f32 {
-        self.conditional_pdfs_integrals[v][u] / self.marginal_pdf_integral[self.height]
+        let pdf_y = self.marginal.pdf(v);
+        let pdf_x = self.conditional[v].pdf(u);
+
+        return pdf_y * pdf_x;
     }
 }
 
