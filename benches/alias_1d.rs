bea69c20b4600f3b67f9b8ad4121815776caf638
ashpil <ashpil@pm.me>
2022-12-24 20:07:20 -0500

    add alias sampling bench

---
+++ benches/alias_1d.rs
@@ -0,0 +1,18 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use pdf_maker::distribution::Distribution1D;
+use pdf_maker::alias::Alias1D;
+
+use rand::{rngs::StdRng, Rng, SeedableRng};
+
+fn alias_1d_build(c: &mut Criterion) {
+    c.bench_function("alias_1d_build", |b| b.iter(|| Alias1D::build(black_box(&[1.0; 1_000_000]))));
+}
+
+fn alias_1d_sample(c: &mut Criterion) {
+    let dist = Alias1D::build(&[1.0; 1_000]);
+    let mut rng = StdRng::seed_from_u64(0);
+    c.bench_function("alias_1d_sample", |b| b.iter(|| dist.sample(rng.gen())));
+}
+
+criterion_group!(benches, alias_1d_build, alias_1d_sample);
+criterion_main!(benches);
