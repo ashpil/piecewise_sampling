03c7d82e0d36d3f6502d6e481d625d48d3f7d006
ashpil <ashpil@pm.me>
2023-01-03 17:23:31 -0500

    sample_continuous for hierarchical + a little more float error resistance

--- src/hierarchical.rs
+++ src/hierarchical.rs
@@ -1,4 +1,7 @@
-use crate::distribution::Distribution1D;
+use crate::distribution::{
+    ContinuousDistribution1D,
+    Distribution1D,
+};
 use num_traits::{
     real::Real,
     Num,
@@ -11,13 +14,16 @@ use num_traits::{
 // remaps u to [0-1) range
 fn select_remap<N: Num + NumCast + PartialOrd + Copy, R: Real>(weights: [N; 2], rand: &mut R) -> (R, u8) {
     let weight_sum = weights[0] + weights[1];
-    let select_prob: R = cast::<N, R>(weights[0]).unwrap() / cast::<N, R>(weight_sum).unwrap();
-    if *rand < select_prob {
-        *rand = *rand / select_prob;
-        (select_prob, 0)
+    let weight_1_r = cast::<N, R>(weights[0]).unwrap();
+    let weight_2_r = cast::<N, R>(weights[1]).unwrap();
+    let weight_sum_r = cast::<N, R>(weight_sum).unwrap();
+    let new_rand: R = *rand * weight_sum_r;
+    if new_rand < weight_1_r {
+        *rand = new_rand / weight_1_r;
+        (weight_1_r / weight_sum_r, 0)
     } else {
-        *rand = (*rand - select_prob) / (R::one() - select_prob);
-        (R::one() - select_prob, 1)
+        *rand = (new_rand - weight_1_r) / weight_2_r;
+        (weight_2_r / weight_sum_r, 1)
     }
 }
 
@@ -105,10 +111,35 @@ impl<R: Real> Distribution1D for Hierarchical1D<R> {
     }
 }
 
+impl<R: Real> ContinuousDistribution1D for Hierarchical1D<R> {
+    fn sample_continuous(&self, mut u: R) -> (R, R) {
+        let mut pdf = self.integral();
+        let mut idx = 0;
+
+        for (i, level) in self.levels.iter().enumerate() {
+            if i > 0 { idx *= 2 }
+            let probs = [
+                get_or_zero(level, idx + 0),
+                get_or_zero(level, idx + 1),
+            ];
+            let (level_pdf, level_idx) = select_remap(probs, &mut u);
+            idx = idx + level_idx as usize;
+            pdf = pdf * level_pdf;
+        }
+        (pdf, (cast::<usize, R>(idx).unwrap() + u) / cast(self.size()).unwrap())
+    }
+
+    fn inverse_continuous(&self, u: R) -> R {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::distribution::distribution_1d_tests;
+    use crate::distribution::continuous_distribution_1d_tests;
 
     distribution_1d_tests!(crate::hierarchical::Hierarchical1D);
+    continuous_distribution_1d_tests!(crate::hierarchical::Hierarchical1D);
 }
 
