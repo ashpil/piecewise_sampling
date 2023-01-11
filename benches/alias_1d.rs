e29a581019a83b48b6993e12b878c687b19ed330
ashpil <ashpil@pm.me>
2023-01-11 14:44:38 -0500

    add coherent alias bench

--- benches/alias_1d.rs
+++ benches/alias_1d.rs
@@ -5,25 +5,45 @@ use discrete_sampling::Alias1D;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 fn alias_1d_build(c: &mut Criterion) {
+    let mut distr = [0.0; 10_000];
+    for (i, weight) in distr.iter_mut().enumerate() {
+        *weight = (i + 1) as f32;
+    }
     c.bench_function("alias_1d_build", |b| b.iter(|| { 
-        let mut distr = [0.0; 10_000];
-        for (i, weight) in distr.iter_mut().enumerate() {
-            *weight = (i + 1) as f32;
-        }
         <Alias1D<f32> as Discrete1D<f32>>::build(black_box(&distr))
     }));
 }
 
-fn alias_1d_sample(c: &mut Criterion) {
+fn alias_1d_sample_coherent(c: &mut Criterion) {
+    let mut distr = [0.0; 100];
+    for (i, weight) in distr.iter_mut().enumerate() {
+        *weight = (i + 1) as f32;
+    }
+    let dist = <Alias1D<f32> as Discrete1D<f32>>::build(&distr);
+    c.bench_function("alias_1d_sample_coherent", |b| b.iter(|| {
+        let sample_count = 1000;
+        for i in 0..sample_count {
+            let i = i as f32 / sample_count as f32;
+            dist.sample(black_box(i));
+        }
+    }));
+}
+
+fn alias_1d_sample_incoherent(c: &mut Criterion) {
     let mut distr = [0.0; 100];
     for (i, weight) in distr.iter_mut().enumerate() {
         *weight = (i + 1) as f32;
     }
     let dist = <Alias1D<f32> as Discrete1D<f32>>::build(&distr);
     let mut rng = StdRng::seed_from_u64(0);
-    c.bench_function("alias_1d_sample", |b| b.iter(|| dist.sample(rng.gen::<f32>())));
+    c.bench_function("alias_1d_sample_incoherent", |b| b.iter(|| {
+        for _ in 0..1000 {
+            let i = rng.gen::<f32>();
+            dist.sample(black_box(i));
+        }
+    }));
 }
 
-criterion_group!(benches, alias_1d_build, alias_1d_sample);
+criterion_group!(benches, alias_1d_build, alias_1d_sample_coherent, alias_1d_sample_incoherent);
 criterion_main!(benches);
 
