510857d56643be480725d68767e0d66b13637b46
ashpil <ashpil@pm.me>
2023-01-11 01:33:53 -0500

    update benches

--- benches/alias_1d.rs
+++ benches/alias_1d.rs
@@ -5,13 +5,23 @@ use discrete_sampling::Alias1D;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 fn alias_1d_build(c: &mut Criterion) {
-    c.bench_function("alias_1d_build", |b| b.iter(|| Alias1D::build(black_box(&[1.0; 1_500_000]))));
+    c.bench_function("alias_1d_build", |b| b.iter(|| { 
+        let mut distr = [0.0; 10_000];
+        for (i, weight) in distr.iter_mut().enumerate() {
+            *weight = (i + 1) as f32;
+        }
+        <Alias1D<f32> as Discrete1D<f32>>::build(black_box(&distr))
+    }));
 }
 
 fn alias_1d_sample(c: &mut Criterion) {
-    let dist = Alias1D::build(&[1.0; 1_000]);
+    let mut distr = [0.0; 100];
+    for (i, weight) in distr.iter_mut().enumerate() {
+        *weight = (i + 1) as f32;
+    }
+    let dist = <Alias1D<f32> as Discrete1D<f32>>::build(&distr);
     let mut rng = StdRng::seed_from_u64(0);
-    c.bench_function("alias_1d_sample", |b| b.iter(|| dist.sample(rng.gen())));
+    c.bench_function("alias_1d_sample", |b| b.iter(|| dist.sample(rng.gen::<f32>())));
 }
 
 criterion_group!(benches, alias_1d_build, alias_1d_sample);
--- benches/inversion_1d.rs
+++ benches/inversion_1d.rs
@@ -5,13 +5,13 @@ use discrete_sampling::Inversion1D;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 fn inversion_1d_build(c: &mut Criterion) {
-    c.bench_function("inversion_1d_build", |b| b.iter(|| Inversion1D::build(black_box(&[1.0; 1_500_000]))));
+    c.bench_function("inversion_1d_build", |b| b.iter(|| <Inversion1D<f32> as Discrete1D<f32>>::build(black_box(&[1.0; 1_500_000]))));
 }
 
 fn inversion_1d_sample(c: &mut Criterion) {
-    let dist = Inversion1D::build(&[1.0; 1_000]);
+    let dist = <Inversion1D<f32> as Discrete1D<f32>>::build(&[1.0; 1_000]);
     let mut rng = StdRng::seed_from_u64(0);
-    c.bench_function("inversion_1d_sample", |b| b.iter(|| dist.sample(rng.gen())));
+    c.bench_function("inversion_1d_sample", |b| b.iter(|| dist.sample(rng.gen::<f32>())));
 }
 
 criterion_group!(benches, inversion_1d_build, inversion_1d_sample);
